a80e6856e97d5fae9f121f23c221607ced50464e
siddharth3562 <siddharthps2503@gmail.com>
2024-09-18 11:29:33 +0530

    data types,operators

---
+++ alert/alert.js
@@ -0,0 +1,15 @@
+
+        // a='ERROR!!'
+        // alert(a)
+
+        // var a
+        // a='hello'
+
+        // let a='hai'
+        // a='hello'
+
+        const a=44
+        // a='hello'
+
+        alert(a)
+
