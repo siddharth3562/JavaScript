7cbde2ac2548f72ff75673ff394b36a97abe2440
siddharth3562 <siddharthps2503@gmail.com>
2024-09-22 18:05:49 +0530

    reg form

---
+++ task/form validation/form.js
@@ -0,0 +1,21 @@
+function valid(){
+    document.getElementById('no.error').innerHTML=''
+    document.getElementById('email.error').innerHTML=''
+    document.getElementById('pin.error').innerHTML=''
+
+    name=document.getElementById('name').value
+    phone=document.getElementById('phno').value
+    email=document.getElementById('email').value
+    pin=document.getElementById('pincode').value
+    console.log(name,phone,email,pin);
+    let firstDigit = phone.charAt(0);
+    if (phone.length!=10 || (firstDigit !=='6' && firstDigit !=='7' && firstDigit !=='8' && firstDigit !=='9')){
+        document.getElementById('no.error').innerHTML='* invalid number'
+    }
+    else if (!/^[a-zA-Z0-9]+@gmail\.com$/.test(email)) {
+        document.getElementById('email.error').innerHTML='* invalid email'
+    }
+    else if(pin.length!=6){
+        document.getElementById('pin.error').innerHTML='* invalid pincode'
+    }
+}
