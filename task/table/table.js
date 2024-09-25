c7e048f53413eec834983f2fadcc9ce4c9f6ea4f
siddharth3562 <siddharthps2503@gmail.com>
2024-09-25 11:29:37 +0530

    table,form task

--- task/table/table.js
+++ task/table/table.js
@@ -1,6 +1,37 @@
-d=[{name:'achu',age:21,place:'ekm'},{name:'manu',age:25,place:'ekm'},{name:'anu',age:45,place:'ekm'},{name:'sani',age:32,place:'ekm'},{name:'mauel',age:50,place:'ekm'},{name:'abhi',age:18,place:'ekm'}]
+d=[{id:'1',name:'achu',age:21,},{id:'2',name:'abhi',age:20,}]
+function display() {
+    const t_var=document.querySelector('#p_table tbody')
+    t_var.innerHTML=''
+    d.forEach(element => {
+        const t_row=document.createElement('tr')
 
-d.forEach(element => {
-    console.log(element);
-    
-});
+        const t_id=document.createElement('td')
+        t_id.textContent=element.id
+        t_row.appendChild(t_id)
+
+        const t_name=document.createElement('td')
+        t_name.textContent=element.name
+        t_row.appendChild(t_name)
+
+        const t_age=document.createElement('td')
+        t_age.textContent=element.age
+        t_row.appendChild(t_age)
+
+        t_var.appendChild(t_row)
+    });
+}
+
+document.getElementById('submit_form').addEventListener('submit',function(event){
+    event.preventDefault() //remove refresh
+    const id=parseInt(document.getElementById('id_no').value)
+    const name=document.getElementById('name').value
+    const age=parseInt(document.getElementById('age').value)
+    d.push({id:id,name:name,age:age})
+    document.getElementById('id_no').value=''
+    document.getElementById('name').value=''
+    document.getElementById('age').value=''
+
+    display()
+})
+
+display()
