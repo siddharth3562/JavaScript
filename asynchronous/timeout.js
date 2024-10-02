b4915ff1669ef66e2a76d416de53c258f1104937
siddharth3562 <siddharthps2503@gmail.com>
2024-10-02 11:33:11 +0530

    timer

--- asynchronous/timeout.js
+++ asynchronous/timeout.js
@@ -28,13 +28,28 @@
 
 // const a=setInterval(display,4000    )
 
-let count=10
-let c=setInterval(function(){
-    console.log(count);
-    count--
-    if (count==0){
-        clearInterval(c)
-        console.log('complete');
+// let count=10
+// let c=setInterval(function(){
+//     console.log(count);
+//     count--
+//     if (count==0){
+//         clearInterval(c)
+//         console.log('complete');
         
-    }
-},1000)
+//     }
+// },1000)
+
+
+function demo(){
+    let a='welcome'
+    console.log(a);
+    const main=document.querySelector('.main')
+    const div=document.createElement('div')
+    // const h2=document.createElement('h2')
+    // h2.textContent=a
+    // div.appendChild(h2)
+    div.innerHTML=`<h2 style='color:green; font-size:50px;'>hello ${a}</h2>`
+    main.appendChild(div)
+}
+
+demo()
--- task/time/time.js
+++ task/time/time.js
@@ -1,16 +1,55 @@
-let minutes='00'
-let seconds=59
+// let minutes=0
+// let seconds=59
 
 
-let c=setInterval(function(){
-    document.getElementById('time').innerHTML=minutes
-    document.getElementById('time').innerHTML=seconds
-    seconds--
-    if (seconds<0){
-        clearInterval(c)
-        console.log('end');
+// let c=setInterval(function(){
+//     document.getElementById('time').innerHTML=minutes
+//     document.getElementById('time').innerHTML=seconds
+//     seconds--
+//     if (seconds<0){
+//         clearInterval(c)
+//         console.log('end');
         
-    }
-},1000)
+//     }
+// },1000)
     
 
+
+// function demo(){
+//     const main=document.querySelector('.main')
+//     const div=document.createElement('div')
+//     let count=59
+//     let min=4
+//     let c=setInterval(function(){
+//         div.innerHTML=`<h2>${min}:${count}</h2>`
+//         count--
+//         if (count==0){
+//             clearInterval(c)
+//             div.innerHTML=`<h2>completed</h2>`
+//         }
+//     },100)
+    
+//     main.appendChild(div)
+// }
+
+// demo()
+
+function demo(){
+    const main=document.querySelector('.main')
+    const div=document.createElement('div')
+    let count=59
+    let min=4
+    let c=setInterval(function(){
+        div.innerHTML=`<h2>${min}:${count}</h2>`
+        count--
+        if (count==0){
+            min--   
+ 
+        }
+
+    },100)
+    
+    main.appendChild(div)
+}
+
+demo()
