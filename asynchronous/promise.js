125fec5ad38fe46fb816d3ad5e42ff47a5cde1e3
siddharth3562 <siddharth3562>
2024-10-07 11:29:22 +0530

    time & async,await

---
+++ asynchronous/promise.js
@@ -0,0 +1,45 @@
+// mypro=new Promise(function(res,rej){
+//     if(10==11){
+//         res('hello')
+//     }
+
+//     else{
+//         rej('hi')
+    
+//      } 
+//      })
+    
+//     mypro.then(function(value) {
+//         console.log(value);
+    
+//     }).catch(function(error) {
+//         console.log(error);
+//     });
+    
+
+
+const main=async (url)=>{
+    try{
+        const response= await fetch(url)
+        const data =await response.json()
+        console.log(data);
+        return [data,null]
+        
+    }catch(error){
+        return [null,error]
+    }
+}    
+
+const fetchdata = async ()=>{
+    const [response,error]= await main('https://dummyjson.com/products')
+    if (response){
+        console.log(response);
+        
+    }
+    else{
+        console.log('error');
+        
+    }
+}
+
+fetchdata()
